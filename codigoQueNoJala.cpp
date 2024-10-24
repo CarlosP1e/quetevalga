4b9f00015e9adb08de85008e3487f06a940b6e26
CarlosP1e <clupiandv@gmail.com>
2024-10-24 03:43:47 +0000

    [FEAT] Ya jala la mamada,ya se puede eligir,

--- codigoQueNoJala.cpp
+++ codigoQueNoJala.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
-using namespace std;
-int main() {
-     cout << "What do you need to convert to?" << endl << "Farenheit to Celcius (1) Or Celcius to Farenheit(2)?" << endl << "Type the option you need: ";
-     int option;
-     cin >> option;
 
-     if (option = 1) {
-          cout << "Type the temperature in Farenheit: ";
-     } else if (option = 2) {
-          cout << "Type the temperature in Celcius";               
-     } else {
-          cout << "Please type in 1 or 2";}
+int main()
+{
+  int option;
+  int temp;
 
-     int temperatureInFarenheit;
-     cin >> temperatureInFarenheit;
-    return 0;
+  do
+  {
+    std::cout << "What do you need to convert to?\n"
+              << "1. Farenheit to Celcius (1)\n"
+              << "2. Celcius to Farenheit (2)\n"
+              << "3. Exit (3)\n\n"
+              << "Type the option you need: ";
+
+    std::cin >> option;
+
+    switch (option)
+    {
+      case 1:
+        std::cout << "Farenheit to Celcius\n"
+                  << "Enter the temperature in Farenheit: ";
+        std::cin >> temp;
+        std::cout << "The temperature in Celcius is: " << (temp - 32) * 5 / 9 << "\n\n";        
+        break;
+
+      case 2:
+        std::cout << "Celcius to Farenheit\n"
+                  << "Enter the temperature in Celcius: ";
+        std::cin >> temp;
+        std::cout << "The temperature in Farenheit is: " << (temp * 9 / 5) + 32 << "\n\n";
+        break;
+
+      case 3:
+        std::exit(0);
+        break;
+
+      default:
+        system("clear");
+        std::cout << "No sea pendejo\n\n\n";
+        break;
     }
+  }
+  while (option != 1 || option != 2);
+
+  return 0;
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+clear
+g++ codigoQueNoJala.cpp -o codigoQueNoJala
+./codigoQueNoJala
