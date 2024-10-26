b9971e7189a2a66b2c470efb751d97a8311176dc
jdi <jmotadv@gmail.com>
2024-10-26 12:11:31 -0700

    [FEAT] PENDEJO ASI DEBISTE HACERLO

--- codigoQueNoJala.cpp
+++ codigoQueNoJala.cpp
@@ -1,47 +1,77 @@
 #include <iostream>
 
+double convertFarenheitToCelcius(double farenheit)
+{
+  return (farenheit - 32) * 5 / 9;
+}
+
+double convertCelciusToFarenheit(double celcius)
+{
+  return (celcius * 9 / 5) + 32;
+}
+
+void clear()
+{
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
+void pause()
+{
+  std::cout << "Press Enter to continue...";
+  std::cin.ignore();
+  std::cin.get();
+}
+
 int main()
 {
-  int option;
-  int temp;
+  char option;
+  int temperature;
 
   do
   {
-    std::cout << "What do you need to convert to?\n"
-              << "1. Farenheit to Celcius (1)\n"
-              << "2. Celcius to Farenheit (2)\n"
-              << "3. Exit (3)\n\n"
-              << "Type the option you need: ";
+    clear();
 
+    std::cout << "What do you need to convert to?\n\n"
+              << "1. Farenheit to Celcius\n"
+              << "2. Celcius to Farenheit\n"
+              << "3. Exit\n\n"
+              << "Type the option you need: ";
     std::cin >> option;
+
+    clear();
 
     switch (option)
     {
-      case 1:
-        std::cout << "Farenheit to Celcius\n"
+      case '1':
+        std::cout << "Farenheit to Celcius\n\n"
                   << "Enter the temperature in Farenheit: ";
-        std::cin >> temp;
-        std::cout << "The temperature in Celcius is: " << (temp - 32) * 5 / 9 << "\n\n";        
+        std::cin >> temperature;
+        std::cout << "The temperature in Celcius is: " << convertFarenheitToCelcius(temperature) << "\n\n";        
         break;
 
-      case 2:
-        std::cout << "Celcius to Farenheit\n"
+      case '2':
+        std::cout << "Celcius to Farenheit\n\n"
                   << "Enter the temperature in Celcius: ";
-        std::cin >> temp;
-        std::cout << "The temperature in Farenheit is: " << (temp * 9 / 5) + 32 << "\n\n";
+        std::cin >> temperature;
+        std::cout << "The temperature in Farenheit is: " << convertCelciusToFarenheit(temperature) << "\n\n";
         break;
 
-      case 3:
+      case '3':
         std::exit(0);
         break;
 
       default:
-        system("clear");
         std::cout << "No sea pendejo\n\n\n";
         break;
     }
+
+    pause();
   }
-  while (option != 1 || option != 2);
+  while (option != '3');
 
   return 0;
 }
